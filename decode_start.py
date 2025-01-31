0f0a2f6478459245764267d89c5ac249c6f0da60
LucasSKuhnke <lucas.kuhnke2005@gmail.com>
2025-01-31 13:22:21 +0100

    i did the contest

--- decode_start.py
+++ decode_start.py
@@ -1,6 +1,13 @@
 alphabet = " abcdefghijklmnopqrstuvwxyz"
 vowel = "aeiou"
 message = ""
+
 with open("secret.txt") as f:
     for line in f:
-        print(line) # instead of print, you should start decoding
+        vowel_count = sum(1 for char in line if char.lower() in vowel)
+        if 0 <= vowel_count < len(alphabet):
+            message += alphabet[vowel_count]
+        else:
+            message += "?"
+
+print("Decoded message:", message)
